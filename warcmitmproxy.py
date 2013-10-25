770493bfacac0ca147d1cd9c134c4db9b0127bce
odie5533 <odiegit@gmail.com>
2013-10-24 22:21:22 -0500

    Added command line options and refactored a bit

--- warcmitmproxy.py
+++ warcmitmproxy.py
@@ -4,6 +4,7 @@
 
 from libmproxy import controller, proxy
 from netlib import http_status
+import argparse
 import os
 import urlparse
 import warcrecords
@@ -21,10 +22,11 @@ class WarcOutputSingleton(object):
         return cls._instance
 
     def __init__(self, filename=None):
-        self.use_gzip = True
-        self.filename = "out.warc.gz"
-        if filename is not None:
-            self.filename = filename
+        if filename is None:
+            filename = "out.warc.gz"
+            print "WarcOutput was not given a filename. Using", filename
+        self.use_gzip = True if args.file.endswith('.gz') else False
+        self.filename = filename
 
         # Make sure init is not called more than once
         try:
@@ -41,13 +43,14 @@ class WarcOutputSingleton(object):
 def constructUrl(msg):
     port = ':'+str(msg.port) if msg.port and msg.port != 80 and msg.port != 443 else ''
     return urlparse.urlunsplit([msg.scheme, msg.host+port, msg.path, '', ''])
+
 def http_version(httpversion):
     return str(httpversion[0]) + '.' + str(httpversion[1])
 
-class StickyMaster(controller.Master):
-    def __init__(self, server):
+class WarcMaster(controller.Master):
+    def __init__(self, filename, server):
+        WarcOutputSingleton(filename)
         controller.Master.__init__(self, server)
-        self.stickyhosts = {}
 
     def run(self):
         try:
@@ -56,7 +59,6 @@ class StickyMaster(controller.Master):
             self.shutdown()
 
     def handle_request(self, msg):
-        hid = (msg.host, msg.port)
         url = constructUrl(msg)
         version = http_version(msg.httpversion)
         headline = ' '.join([msg.method, url, 'HTTP/'+version])
@@ -67,21 +69,39 @@ class StickyMaster(controller.Master):
         msg.reply()
 
     def handle_response(self, msg):
-        hid = (msg.request.host, msg.request.port)
         version = http_version(msg.httpversion)
         h_status = 'HTTP/'+version+' '+str(msg.code)+' '+http_status.RESPONSES[msg.code]
         block = h_status + '\r\n' + str(msg.headers) + '\r\n' +msg.content
         url = constructUrl(msg.request)
-        record = warcrecords.WarcResponseRecord(url=url,
-                                                block=block)
+        record = warcrecords.WarcResponseRecord(url=url, block=block)
         WarcOutputSingleton().write_record(record)
         
         msg.reply()
 
+parser = argparse.ArgumentParser(description='Warc Man-in-the-Middle Proxy')
+parser.add_argument('--pem', default='ca.pem',
+                    help='Privacy-enhanced Electronic Mail file.')
+parser.add_argument('-p', '--port', default=8000,
+                    help='Port to run the proxy server on.')
+parser.add_argument('-f', '--file', default='out.warc.gz',
+                    help='WARC filename to save to. Include .gz for gzip.')
 
+args = parser.parse_args()
+args.pem = os.path.expanduser(args.pem)
+if not os.path.isfile(args.pem):
+    print "Supplied pem file can not be found:", args.pem
+    exit()
+try: # Parse the port into an integer
+    args.port = int(args.port)
+except:
+    print "Please specify a valid number for the port:", args.port
+    exit()
+
+# Create proxy config from PEM file
 config = proxy.ProxyConfig(
-    cacert = os.path.expanduser("ca.pem")
+    cacert = os.path.expanduser(args.pem)
 )
-server = proxy.ProxyServer(config, 8000)
-m = StickyMaster(server)
+server = proxy.ProxyServer(config, args.port)
+m = WarcMaster(args.file, server)
+print "Proxy server running on port", args.port
 m.run()
